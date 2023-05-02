214e670b391e94a3fe29773bc551ca408ab6f786
Andy-BB <81426626+Andy-BB@users.noreply.github.com>
2023-05-02 09:18:11 +0700

    Add files via upload

---
+++ hitung_orang.php
@@ -0,0 +1,25 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$dbname = "Redlock";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT COUNT(*) as total FROM users";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+  $row = $result->fetch_assoc();
+  echo "Total number of users: " . $row["total"];
+} else {
+  echo "0 results";
+}
+
+$conn->close();
+
+?>
